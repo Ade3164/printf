994be9dcd72cbe91625312b022157db6fdf86ff0
Ade3164 <adebayoidris051@gmail.com>
2022-04-16 21:44:04 +0100

    first

--- printf_const.c
+++ printf_const.c
@@ -1,52 +1,49 @@
-#include <limits.h>
-#include <stdio.h>
 #include "main.h"
-#include <stdarg.h>
 /**
- * _printf - prints formatted data to stdout
- * @format: string that contains the format to print
- * Return: number of characters written
+ * _printf - replicate prinft
+ *
+ * @format: format printf
+ * Return: Always 0
  */
-int _printf(char *format, ...)
+int _printf(const char *format, ...)
 {
-	int written = 0, (*structype)(char *, va_list);
-	char q[3];
-	va_list pa;
 
-	if (format == NULL)
+	int (*printer)(va_list);
+	int length = 0;
+	va_list content;
+
+	va_start(content, format);
+
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	q[2] = '\0';
-	va_start(pa, format);
-	_putchar(-1);
-	while (format[0])
+
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+
+	for (; *format; format++)
 	{
-		if (format[0] == '%')
+		if (*format == '%')
 		{
-			structype = driver(format);
-			if (structype)
+			format++;
+
+			printer = get_print(*format);
+
+			if (printer)
 			{
-				q[0] = '%';
-				q[1] = format[1];
-				written += structype(q, pa);
-			}
-			else if (format[1] != '\0')
-			{
-				written += _putchar('%');
-				written += _putchar(format[1]);
+				length += printer(content);
 			}
 			else
 			{
-				written += _putchar('%');
-				break;
+				length += _putchar('%');
+				length += _putchar(*format);
 			}
-			format += 2;
 		}
 		else
 		{
-			written += _putchar(format[0]);
-			format++;
+			length += _putchar(*format);
 		}
 	}
-	_putchar(-2);
-	return (written);
+	va_end(content);
+
+	return (length);
 }
