54bce4e2c77c972616d2a7184b645a47872f2820
Ade3164 <adebayoidris051@gmail.com>
2022-04-17 07:35:02 +0100

    print

--- print.c
+++ print.c
@@ -1,52 +1,28 @@
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-#include <stddef.h>
 /**
- * _printf - recreates the printf function
- * @format: string with format specifier
- * Return: number of characters printed
+ * _printf - prints according to format
+ * @format: The given format
+ *
+ * Return: On success 1.
  */
 int _printf(const char *format, ...)
 {
-	if (format != NULL)
-	{
-		int count = 0, i;
-		int (*m)(va_list);
-		va_list args;
+	va_list list;
+	int i = 0, length = 0;
 
-		va_start(args, format);
-		i = 0;
-		if (format[0] == '%' && format[1] == '\0')
-			return (-1);
-		while (format != NULL && format[i] != '\0')
+	if (format == NULL || (format[0] == '%' && !format[1]))
+		return (-1);
+	va_start(list, format);
+		for (i = 0; format[i]; i++)
 		{
 			if (format[i] == '%')
 			{
-				if (format[i + 1] == '%')
-				{
-					count += _putchar(format[i]);
-					i += 2;
-				}
-				else
-				{
-					m = get_func(format[i + 1]);
-					if (m)
-						count += m(args);
-					else
-						count = _putchar(format[i]) + _putchar(format[i + 1]);
-					i += 2;
-				}
-			}
-			else
-			{
-				count += _putchar(format[i]);
+				length += get_print_func(format[i + 1], list);
 				i++;
 			}
+			else
+				length += _putchar(format[i]);
 		}
-		va_end(args);
-		return (count);
-	}
-	return (-1);
+	va_end(list);
+	return (length);
 }
