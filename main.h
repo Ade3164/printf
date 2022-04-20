6ef2cbb29b21396616d63de29169c8ef7bd30d80
Ade3164 <adebayoidris051@gmail.com>
2022-04-20 04:50:24 +0100

    print

--- main.h
+++ main.h
@@ -1,42 +1,30 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef  MAINH
+#define MAINH
 
-/**
- * struct printHandler - struct for agrupate
- * %d, %c, etc... formatters
- * @c: character
- * @f: character func
- *
- */
-typedef struct printHandler
-{
-	char c;
-	int (*f)(va_list);
-} ph;
-
-int _printf(const char *format, ...);
-int (*get_print(char c))(va_list);
+#include <stdarg.h>
 int _putchar(char c);
-int _puts(char *str);
-char *string_to_base(unsigned long int num, int base, bool uppercase);
-/** prints text*/
-int print_char(va_list);
-int print_string(va_list);
-int print_reverse_string(va_list);
-/** print mememory address */
-int print_address(va_list);
-/** prints nums*/
-int print_int(va_list);
-int print_unsigned(va_list);
-/** prints basics*/
-int print_hexa(va_list);
-int print_octal(va_list);
-int print_binary(va_list);
-int print_percent(va_list);
-int print_hexa_in_uppercase(va_list);
-/** utilities */
-int count_digits(int i);
-void print_number(int n);
-void print_rev_recursion(char *s);
+int _printf(const char *format, ...);
+int print_char(va_list c);
+int print_string(va_list s);
+int print_int(va_list i);
+int print_dec(va_list d);
+int print_rev(va_list r);
+int print_bin(va_list b);
+int print_unsig(va_list u);
+int print_octal(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_rot13(va_list R);
+/**
+  * struct code_format - Struct format
+  *
+  * @sc: The specifiers
+  * @f: The function associated
+  */
+typedef struct code_format
+{
+	char *sc;
+	int (*f)(va_list);
+} code_f;
 
-#endif
+#endif /* HOLBERTONH */
